a173fda8ff9d6248a44466490ad948ec6237bcf7
rubyrubenstahl <ruby@ruby-lighting.com>
2019-11-26 10:29:06 -0500

    Removde dead code

--- example/src/main.js
+++ example/src/main.js
@@ -1,9 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
-import ReactiveProvide from "vue-reactive-provide";
 
 Vue.config.productionTip = false;
-Vue.use(ReactiveProvide);
 import FeathersVueRx from "../../src";
 Vue.use(FeathersVueRx)
 new Vue({
