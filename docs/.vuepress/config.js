5f96aabadd15959bbe000266b4770b591705fff9
rubyrubenstahl <ruby@ruby-lighting.com>
2019-11-26 11:04:07 -0500

    Update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,17 @@
 module.exports = {
   title: "Feathers Vue Rx",
+  repo: "RubyRubenstahl/feathers-vue-rx",
+  docsDir: "docs",
   description: "Declarative, Reactive Vue components for Feathers",
   themeConfig: {
     sidebar: [
-      '/',
-      'getting-started',
-      'feathers-app',
-      'feathers-find',
-      'feathers-get',
-      'feathers-authenticated',
-      'feathers-unauthenticated'
-    ],
+      "/",
+      "getting-started",
+      "feathers-app",
+      "feathers-find",
+      "feathers-get",
+      "feathers-authenticated",
+      "feathers-unauthenticated"
+    ]
   }
 };
